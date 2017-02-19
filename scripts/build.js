844af1b2b933997cb5e63d2edd8705315f74334a
Adam Harris <aharris88@users.noreply.github.com>
2017-02-18 22:55:30 -0700

    Add createBooks in build script

--- scripts/build.js
+++ scripts/build.js
@@ -3,14 +3,21 @@ const fs = require('fs');
 const SOURCE_DIRECTORY = 'src/languages';
 const OUTPUT_DIRECTORY = 'dist'
 const LANGUAGE_FILE = `${OUTPUT_DIRECTORY}/languages.json`;
+const getBooksFile = (lang) => `${OUTPUT_DIRECTORY}/${lang}/books.json`;
+
+const languages = fs.readdirSync(SOURCE_DIRECTORY);
 
 const createLanguages = () => {
-  fs.readdir(SOURCE_DIRECTORY, (err, languages) => {
-    const json = {languages};
-    fs.writeFile(LANGUAGE_FILE, JSON.stringify(json), (err) => {
-      if (err) throw err;
-    });
+  const output = JSON.stringify({languages});
+  fs.writeFileSync(LANGUAGE_FILE, output);
+};
+
+const createBooks = () => {
+  languages.map((lang) => {
+    // TODO: get output from src/en/niv books function
+    fs.writeFileSync(getBooksFile(lang), 'test');
   });
 };
 
 createLanguages();
+createBooks();
